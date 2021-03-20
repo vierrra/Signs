41e743714a5f7e453c13e7be49b23364aa07048c
Renato <renato.vieira@stant.com.br>
2021-03-20 15:00:19 -0300

    Initial Commit

---
+++ Signos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Signos
+//
+//  Created by Renato on 3/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
